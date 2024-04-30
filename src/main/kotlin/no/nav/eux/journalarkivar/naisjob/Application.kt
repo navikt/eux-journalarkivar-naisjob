ebe0e06d82afb00046cab34acdee336d4cf77b75
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2024-04-30 13:59:20 +0200

    journalarkivar

--- src/main/kotlin/no/nav/eux/journalarkivar/naisjob/Application.kt
+++ src/main/kotlin/no/nav/eux/journalarkivar/naisjob/Application.kt
@@ -17,6 +17,7 @@ class Application(
         log.info { "ferdigstiller journalposter.." }
         client.ferdigstillJournalposter()
         log.info { "journalposter ferdigstilt" }
+        client.execute()
     }
 }
 
--- src/main/kotlin/no/nav/eux/journalarkivar/naisjob/integration/journalarkivar/EuxJournalarkivarClient.kt
+++ src/main/kotlin/no/nav/eux/journalarkivar/naisjob/integration/journalarkivar/EuxJournalarkivarClient.kt
@@ -1,5 +1,6 @@
 package no.nav.eux.journalarkivar.naisjob.integration.journalarkivar
 
+import io.github.oshai.kotlinlogging.KotlinLogging.logger
 import no.nav.eux.journalarkivar.naisjob.integration.config.post
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
@@ -9,10 +10,28 @@ import org.springframework.web.client.RestTemplate
 
 @Component
 class EuxJournalarkivarClient(
+    @Value("\$arkivarprosess")
+    val arkivarprosess: String,
     @Value("\${endpoint.euxjournalarkivar}")
     val euxJournalarkivarUrl: String,
     val euxJournalarkivarRestTemplate: RestTemplate
 ) {
+
+    val log = logger {}
+
+    fun execute() {
+        if (arkivarprosess == "feilregistrer") {
+            euxJournalarkivarRestTemplate
+                .post()
+                .uri("${euxJournalarkivarUrl}/api/v1/arkivarprosess/$arkivarprosess/execute")
+                .contentType(APPLICATION_JSON)
+                .accept(MediaType.ALL)
+                .retrieve()
+                .toBodilessEntity()
+        } else {
+            log.info { "Arkivarprosess: $arkivarprosess" }
+        }
+    }
 
     fun ferdigstillJournalposter() {
         euxJournalarkivarRestTemplate
